96afa3029b666ec49f11847c5a8c398cbdb7682d
yossi126 <yossi126@gmail.com>
2023-11-25 02:52:04 +0200

    finished

--- mynotes/frontend/src/App.js
+++ mynotes/frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import NoteListPage from "./pages/NoteListPage";
 import NotePage from "./pages/NotePage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
